73fce75e3370e0203054a445cb1ac4b9723b4244
JoabFelton138 <joab_felton91@hotmail.co.uk>
2023-09-06 21:14:22 +0100

    success alert pops on submission

--- c-lewis-window-cleaning/src/components/Form.jsx
+++ c-lewis-window-cleaning/src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 
 import '../CSS/Form.css'
 
@@ -7,12 +7,12 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
-import {RenderAlert} from './commons/Alert';
+import { RenderAlert } from './commons/Alert';
 
 import emailjs from '@emailjs/browser';
 
 import { validateSubmission } from './commons/utils';
-import {isValidString} from './commons/validation';
+import { isValidString } from './commons/validation';
 import { messages } from './commons/messages';
 
 const ContactForm = () => {
@@ -25,22 +25,23 @@ const ContactForm = () => {
     const [postCode, setPostCode] = useState("");
     const [area, setArea] = useState("");
     const [message, setMessage] = useState("");
-    
+
     const [HDYFOAU, setHDYFOAU] = useState("");
     const [recommendationChecked, setRecommendationChecked] = useState(false);
     const [isChecked, setIsChecked] = useState(false);
     const [socialMediaChecked, setSocialMediaChecked] = useState(false);
-    
+
+    const [submissionSuccessful, setSubmissionSuccessful] = useState(false);
+
     const [validated, setValidated] = useState(false);
-    const [invalidInput, setInvalidInput] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
+
     const form = useRef();
 
     const handleRecommendationCheck = () => {
 
         setRecommendationChecked(true);
 
-        if (isChecked){
+        if (isChecked) {
             setIsChecked(!isChecked);
         }
 
@@ -48,7 +49,7 @@ const ContactForm = () => {
             setSocialMediaChecked(!socialMediaChecked);
         }
 
-        else{
+        else {
             setRecommendationChecked(!recommendationChecked);
         }
 
@@ -56,14 +57,14 @@ const ContactForm = () => {
     }
 
     const handleIsChecked = () => {
-        
+
         setIsChecked(true);
 
         if (recommendationChecked) {
             setRecommendationChecked(!recommendationChecked);
         }
 
-        else if (socialMediaChecked){
+        else if (socialMediaChecked) {
             setSocialMediaChecked(!socialMediaChecked);
         }
 
@@ -83,7 +84,7 @@ const ContactForm = () => {
             setRecommendationChecked(!recommendationChecked);
         }
 
-        else if (isChecked){
+        else if (isChecked) {
             setIsChecked(!isChecked);
         }
 
@@ -94,35 +95,25 @@ const ContactForm = () => {
         setHDYFOAU("Social Media");
     }
 
-    // const handleSubmit = (event) => {
-        
-    //     const form = event.currentTarget;
-    //     let errorMessage = validateSubmission(firstName);
-    //     setHDYFOAU()
-    //     setErrorMessage(errorMessage);
+    const sendEmail = (e) => {
+        e.preventDefault();
+        setSubmissionSuccessful(false);
 
-    //     console.log(errorMessage);
-        
-    //     if (form.checkValidity() === false) {
-    //         event.preventDefault();
-    //         event.stopPropagation();
-    //     }
-      
-    //     setValidated(true);
-    // }
+        emailjs.sendForm('service_b7pfg3u', 'template_bpuo3nh', form.current, 'bR_vQvzEQNeW8qqkX')
+            .then((result) => {
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+                console.log(result.text);
 
-    emailjs.sendForm('service_b7pfg3u', 'template_bpuo3nh', form.current, 'bR_vQvzEQNeW8qqkX')
-      .then((result) => {
-          console.log(result.text);
-          console.log("message sent");
-          e.target.reset();
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+                if (result.text === "OK") {
+                    setSubmissionSuccessful(true);
+                }
+                e.target.reset();
+            },
+
+                (error) => {
+                    console.log(error.text);
+                });
+    };
 
     return (
         <div className="form-container border border-1 rounded">
@@ -130,78 +121,114 @@ const ContactForm = () => {
                 <h3>Need a Quote?</h3>
                 <p className="mt-2">If you would like a free quote, fill out the form and i'll get back to you!</p>
             </div>
-            <Form ref={form} onSubmit={sendEmail} className="p-4" style={{width: '55%;'}}>
+            <Form ref={form} onSubmit={sendEmail} className="p-4">
+                {submissionSuccessful &&
+                    <Alert variant="success">
+                        <Alert.Heading>Thanks for getting in touch!</Alert.Heading>
+                        <p className="mb-0">
+                            I really appreciate you reaching out.
+                            I will get back in touch with you soon!
+                            Have a great day!
+                        </p>
+                    </Alert>}
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Label>First Name*</Form.Label>
+                        <Form.Control
+                            placeholder="First name"
+                            name="first_name" />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid First Name.
+                        </Form.Control.Feedback>
+                    </Col>
+                    <Col>
+                        <Form.Label>Surname*</Form.Label>
+                        <Form.Control
+                            placeholder="Last Name"
+                            name="surname" />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Surname.
+                        </Form.Control.Feedback>
+                    </Col>
+                </Row>
 
-            <Row className="mb-3">
-                <Col>
-                    <Form.Label>First Name*</Form.Label>
-                    <Form.Control 
-                        placeholder="First name" 
-                        name="first_name"/>
-                </Col>
-                <Col>
-                    <Form.Label>Surname*</Form.Label>
-                    <Form.Control 
-                        placeholder="Last Name" 
-                        name="surname"/>
-                </Col>
-            </Row>
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Label>Phone Number*</Form.Label>
+                        <Form.Control
+                            placeholder="Phone Number"
+                            name="number" />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Phone Number.
+                        </Form.Control.Feedback>
+                    </Col>
+                    <Col>
+                        <Form.Label>Email*</Form.Label>
+                        <Form.Control
+                            placeholder="Email"
+                            name="user_email"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Surname.
+                        </Form.Control.Feedback>
+                    </Col>
+                </Row>
 
-            <Row className="mb-3">
-                <Col>
-                    <Form.Label>Phone Number*</Form.Label>
-                    <Form.Control 
-                        placeholder="Phone Number" 
-                        name="number"/>
-                </Col>
-                <Col>
-                    <Form.Label>Email*</Form.Label>
-                    <Form.Control 
-                        placeholder="Email"
-                        name="user_email" 
-                    />
-                </Col>
-            </Row>
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Label>Address*</Form.Label>
+                        <Form.Control
+                            placeholder="Address"
+                            name="address" />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Address.
+                        </Form.Control.Feedback>
+                    </Col>
+                </Row>
 
-            <Row className="mb-3">
-                <Col>
-                    <Form.Label>Address*</Form.Label>
-                    <Form.Control 
-                        placeholder="Address" 
-                        name="address"/>
-                </Col>
-            </Row>
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Label>Post Code*</Form.Label>
+                        <Form.Control
+                            placeholder="Post Code"
+                            name="post_code" />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Post Code.
+                        </Form.Control.Feedback>
+                    </Col>
+                    <Col>
+                        <Form.Label>Area*</Form.Label>
+                        <Form.Control
+                            placeholder="Area"
+                            name="area"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Area.
+                        </Form.Control.Feedback>
+                    </Col>
+                </Row>
 
-            <Row className="mb-3">
-                <Col>
-                    <Form.Label>Post Code*</Form.Label>
-                    <Form.Control 
-                        placeholder="Post Code" 
-                        name="post_code"/>
-                </Col>
-                <Col>
-                    <Form.Label>Area*</Form.Label>
-                    <Form.Control 
-                        placeholder="Area"
-                        name="area" 
-                    />
-                </Col>
-            </Row>
-
-            <Row className='mb-3'>
-                <Col>
-                    <Form.Label>Message*</Form.Label>
-                    <Form.Control 
-                        placeholder="Message"
-                        as="textarea" 
-                        rows={3} 
-                        name="message" 
-                    />
-                </Col>
-            </Row>
-            <Button type="submit" value="Send" variant="secondary">
-                Submit
-            </Button>{' '}
+                <Row className='mb-3'>
+                    <Col>
+                        <Form.Label>Message*</Form.Label>
+                        <Form.Control
+                            placeholder="Message"
+                            as="textarea"
+                            rows={3}
+                            name="message"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a message.
+                        </Form.Control.Feedback>
+                    </Col>
+                </Row>
+                <Button
+                    type="submit"
+                    value="Send"
+                    variant="secondary"
+                    className="mt-2">
+                    Submit
+                </Button>{' '}
             </Form>
         </div>
     );
