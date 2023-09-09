85c1afb78945056dd593748c9472e111c16f360e
JoabFelton138 <joab_felton91@hotmail.co.uk>
2023-09-09 15:48:32 +0100

    Portfolio code replaced with bs grid

--- c-lewis-window-cleaning/src/components/Home/Portfolio.jsx
+++ c-lewis-window-cleaning/src/components/Home/Portfolio.jsx
@@ -9,6 +9,10 @@ import wc from '../../images/gallery/wc5.jpg'
 import bwc from '../../images/gallery/bwc.jpg'
 
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import Image from 'react-bootstrap/Image';
 
 const Portfolio = () => {
     return (<div className="portfolio-container">
@@ -16,41 +20,43 @@ const Portfolio = () => {
             <h3>MY PORTFOLIO</h3>
             <h6>I take great pride in my work, see for yourself!</h6>
         </div>    
-        <div className="inner-container row pt-5 d-flex justify-content-center">
-            <div className="images col-lg-4 col-md-12 mb-3">
-                <figure>
-                    <img
-                        src={cc}
-                        className="w-100 shadow-1-strong mb-3"
-                        alt="Boat on Calm Water"
-                    />
-                </figure>
-                <figure>
-                    <img
-                        src={gutter}
-                        className="w-100 shadow-1-strong  mb-3"
-                        alt="Wintry Mountain Landscape"
-                    />
-                </figure>
-            </div>
+        <Container>
+            <Row className="pt-5 d-flex justify-content-center">
+                <Col lg={4} md={12} mb={3}>
+                    <figure>
+                        <Image
+                            src={cc}
+                            className="w-100 shadow-1-strong mb-3"
+                            alt="Boat on Calm Water"
+                        />
+                    </figure>
+                    <figure>
+                        <Image
+                            src={gutter}
+                            className="w-100 shadow-1-strong  mb-3"
+                            alt="Wintry Mountain Landscape"
+                        />
+                    </figure>
+                </Col>
+                <Col lg={4} md={12} mb={3}>
+                    <figure>
+                        <Image
+                            src={pw}
+                            className="w-100 shadow-1-strong  mb-3"
+                            alt="Mountains in the Clouds"
+                        />
+                    </figure>
+                    <figure>
+                        <Image
+                            src={bwc}
+                            className="w-100 shadow-1-strong  mb-3"
+                            alt="Boat on Calm Water"
+                        />
+                    </figure>
+                </Col>
+            </Row>
+        </Container>
 
-            <div className="images col-lg-4 mb-4 mb-lg-0">
-                <figure>
-                    <img
-                        src={pw}
-                        className="w-100 shadow-1-strong  mb-3"
-                        alt="Mountains in the Clouds"
-                    />
-                </figure>
-                <figure>
-                    <img
-                        src={bwc}
-                        className="w-100 shadow-1-strong  mb-3"
-                        alt="Boat on Calm Water"
-                    />
-                </figure>
-            </div>
-        </div>
         <Button className="portfolio-button" variant="outline-secondary" >VIEW PORTFOLIO</Button>{' '}
     </div>);
 }
