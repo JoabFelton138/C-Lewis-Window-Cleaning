e627f31b00df3fc4188a4f9dd755a63e9aec9358
JoabFelton138 <joab_felton91@hotmail.co.uk>
2023-10-09 13:32:43 +0100

    hero img responsive

--- c-lewis-window-cleaning/src/components/Services/Hero.jsx
+++ c-lewis-window-cleaning/src/components/Services/Hero.jsx
@@ -19,18 +19,16 @@ const Hero = (props) =>{
     let x = heroImages[props.source];
 
     return(
-        <div className='hero-container'  
-          
+            <div className='hero-container'
              style={{
                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), 
                                  rgba(0, 0, 0, 0.4)), 
                                  url(${x.src})`,
-             }}
-        >
-
-            <div className="hero-text">
-                <h1>WEST MIDLANDS WINDOW CLEANING</h1>
-            </div>
+                                 height: '90vh'
+             }}>
+                <div className="hero-text">
+                    <h1>WEST MIDLANDS WINDOW CLEANING</h1>
+                </div>
         </div>
     );
 }
