7a60c3ebf195257a2a955820c769a1826b29ab48
JoabFelton138 <joab_felton91@hotmail.co.uk>
2023-11-20 21:07:12 +0000

    Added imgs that redirect to fb, google and nd reviews.

--- c-lewis-window-cleaning/src/pages/TestimonalsPage.jsx
+++ c-lewis-window-cleaning/src/pages/TestimonalsPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Hero from '../components/Services/Hero';
 import ReviewCarousel from '../components/Testimonials/ReviewCarousel';
-import TestCarousel from '../components/Testimonials/TestCarousel';
 import styled from 'styled-components'
+import Image from 'react-bootstrap/Image';
+
+import fb from '../images/FB.png';
+import google from '../images/GOOGLE.png';
+import nd from '../images/NEXTDOOR.png';
 
 const Container = styled.div`
     text-align: center;
@@ -18,6 +22,17 @@ const Content = styled.p`
     padding-top: 2vw;
 `
 
+const ReviewImage = styled(Image)`
+    width: 60px;
+    padding-top: 1vw;
+    cursor: pointer;
+    transition: opacity 0.25s;
+
+    &:hover{
+        opacity: 0.7;
+    }
+`
+
 export default function TestimonialsPage (){
 
     
@@ -27,6 +42,9 @@ export default function TestimonialsPage (){
             <Container>
                 <Header>REVIEWS</Header>
                 <Content>I work very hard for my customers and it shows in my reviews, take a look!</Content>
+                <ReviewImage src={fb} alt="fb" onClick={() => window.open("https://www.facebook.com/WCS.CLewis", "_blank")}/>
+                <ReviewImage src={google} alt="google" onClick={() => window.open("https://www.google.com/maps/place/C.Lewis+Window+Cleaning+Services/@52.6477151,-2.4485995,11z/data=!4m8!3m7!1s0xadb1940461bbdf07:0x8d63bb8f6fac1264!8m2!3d52.6478294!4d-2.2837939!9m1!1b1!16s%2Fg%2F11tfgwspw1?entry=ttu", "_blank")}/>
+                <ReviewImage src={nd} alt="nd" onClick={() => window.open("https://nextdoor.co.uk/pages/clewis-window-cleaning-services/", "_blank")}/>
                 <ReviewCarousel/>
             </Container>
         </div>
